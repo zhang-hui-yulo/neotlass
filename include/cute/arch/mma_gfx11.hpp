27e957da4e13d19458bec183794e82e96b019975
Zhang Hui <77381088+zhang-hui-yulo@users.noreply.github.com>
2025-02-03 15:15:27 +0800

    update gfx 11 fp16 mma opsel

--- include/cute/arch/mma_gfx11.hpp
+++ include/cute/arch/mma_gfx11.hpp
@@ -14,6 +14,7 @@ namespace cute {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // MMA 16x16x16 TN
+template <bool opsel = false>
 struct GFX11_16x16x16_F16F16F16F16_TN
 {
   typedef _Float16 half_t16 __attribute__((ext_vector_type(16)));
@@ -29,7 +30,7 @@ struct GFX11_16x16x16_F16F16F16F16_TN
       half_t16   const& c0)
   {
 #if defined(CUTE_ARCH_MMA_GFX11_ENABLED)
-    d0 = __builtin_amdgcn_wmma_f16_16x16x16_f16_w32(a0, b0, c0, false);
+    d0 = __builtin_amdgcn_wmma_f16_16x16x16_f16_w32(a0, b0, c0, opsel);
 #else
     CUTE_INVALID_CONTROL_PATH("Attempting to use GFX10_16x16x16_F16F16F16F16_TN without CUTE_ARCH_MMA_GFX10_ENABLED");
 #endif
--- include/cute/atom/mma_traits_gfx11.hpp
+++ include/cute/atom/mma_traits_gfx11.hpp
@@ -16,6 +16,19 @@ namespace {
 using GFX11_16x16_Row = Layout<Shape <Shape <_16,_2>,Shape < _16>>,
                                Stride<Stride< _1,_0>,Stride< _16>>>;
 
+template <bool opsel>
+struct CUTE_ALIGNAS(4) GFX11FrgTypeAccum {
+    half_t data[2];
+
+    CUTE_HOST_DEVICE constexpr operator half_t() const noexcept {
+        if constexpr (!opsel) {
+            return data[0];
+        } else {
+            return data[1];
+        }
+    }
+};
+
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -23,21 +36,14 @@ using GFX11_16x16_Row = Layout<Shape <Shape <_16,_2>,Shape < _16>>,
 ///////////////////////////////////////////////////////////////////////////////
 
 template <>
-struct MMA_Traits<GFX11_16x16x16_F16F16F16F16_TN>
+struct MMA_Traits<GFX11_16x16x16_F16F16F16F16_TN<false>>
 {
     using ValTypeD = half_t;
     using ValTypeA = half_t;
     using ValTypeB = half_t;
     using ValTypeC = half_t;
 
-    struct CUTE_ALIGNAS(4) FrgTypeAccum {
-        half_t value;
-        half_t empty;
-
-        CUTE_HOST_DEVICE constexpr operator half_t() const noexcept { return value; }
-    };
-
-    using FrgTypeC = FrgTypeAccum;
+    using FrgTypeC = GFX11FrgTypeAccum<false>;
 
     using Shape_MNK = Shape<_16, _16, _16>;
     using ThrID = Layout<_32>;
@@ -45,6 +51,12 @@ struct MMA_Traits<GFX11_16x16x16_F16F16F16F16_TN>
     using BLayout = GFX11_16x16_Row;
     using CLayout = Layout<Shape <Shape <_16, _2>, Shape < _8>>,
                            Stride<Stride<_16, _1>, Stride< _2>>>;
+};
+
+template <>
+struct MMA_Traits<GFX11_16x16x16_F16F16F16F16_TN<true>>
+     : MMA_Traits<GFX11_16x16x16_F16F16F16F16_TN<false>> {
+    using FrgTypeC = GFX11FrgTypeAccum<true>;
 };
 
 }
